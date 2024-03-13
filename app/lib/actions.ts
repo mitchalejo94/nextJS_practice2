c92206a1c0561ca5d0bdc5a23fbcbe3f678e14a8
Mitchel Alejo <michalejo94@gmail.com>
2024-03-13 13:32:03 -0500

    Error Handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,11 +27,16 @@ const amountInCents = amount * 100;
 //create a new date with the format "YYYY-MM-DD" for the invoice's creation date
 const date = new Date().toISOString().split('T')[0];
 
+try {
 //create an SQL query to insert the new invoice into your database and pass in the variables
 await sql`
 INSERT INTO invoices (customer_id, amount, status, date)
 VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-`;
+`;}catch(error){
+  return {
+    message: 'Database Error: Failed to Create Invoice.',
+  };
+};
 //revalidatePath allows you to purge cached data on-demand for a specific path.
 revalidatePath('/dashboard/invoices');
 
@@ -58,11 +63,16 @@ export async function updateInvoice(id: string, formData: FormData) {
    
     const amountInCents = amount * 100;
    
-    await sql`
+    try{
+
+      await sql`
       UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
-    `;
+      `;
+    }catch (error) {
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -70,7 +80,13 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 
   export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+    throw new Error('Failed to Delete Invoice');
+    try{
+
+      await sql`DELETE FROM invoices WHERE id = ${id}`;
+      revalidatePath('/dashboard/invoices');
+    }catch(error){
+      return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
     //Calling revalidatePath will trigger a new server request and re-render the table.
   }
